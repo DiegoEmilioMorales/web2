894ff33163e1de94cd3dbd976bf256276053e627
DiegoEmilioMorales <146789204+DiegoEmilioMorales@users.noreply.github.com>
2023-10-03 20:59:56 +0000

    added dynamic data and API

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+jobs_l = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'India',
+    'salary': 'rs.1000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'India',
+    'salary': 'rs.1500'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'rs.1200'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco',
+    'salary':'rs.1900'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                        jobs=jobs_l)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(jobs_l)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
